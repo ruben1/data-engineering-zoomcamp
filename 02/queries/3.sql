b26872be19b9f9125ba2daab89c8935acf677578
Ruben Vicario Gonzalez <ruben1586@gmail.com>
2025-01-28 15:22:32 +0100

    week 2

---
+++ 02/queries/3.sql
@@ -0,0 +1,3 @@
+SELECT COUNT(*)
+FROM `dtc-de-course-447007.de_zoomcamp.yellow_tripdata`
+WHERE filename LIKE 'yellow_tripdata_2020%'
---
+++ 02/queries/4.sql
@@ -0,0 +1,3 @@
+SELECT COUNT(*)
+FROM `dtc-de-course-447007.de_zoomcamp.green_tripdata`
+WHERE filename LIKE 'green_tripdata_2020%'
